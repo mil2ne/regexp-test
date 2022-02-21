b83df448b21556ed760f6eae8786270b5240aeee
mil2ne <show3070@gmail.com>
2022-02-21 20:12:17 +0900

    regexp practice-4

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 let str = `
-010-1234-5678.
+010-1234-5678
 thesecon@gmail.com
-http://www.omdbapi.com/?apikey=70ccc60c&s=frozen
+https://www.omdbapi.com/?apikey=70ccc60c&s=frozen
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
+http://localhost:1234
 `
 
 // const regexp = new RegExp('the','gi')
 // console.log(str.match(regexp)) // (3) ['the', 'The', 'the']
 
-console.log(str.match(/\.$/gim))
+console.log(
+  str.match(/h..p/g)
+  )
+
+console.log(
+  str.match(/fox|dog/g)
+  )
+
+console.log(
+  str.match(/https?/g)
+  )
+
+console.log(
+  str.match(/\b\w{2,3}\b/g)
+  )
+
