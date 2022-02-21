8d0e899e9068c125c27182a7c91fabebcde1e45f
mil2ne <show3070@gmail.com>
2022-02-21 19:50:41 +0900

    regexp practice-3

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let str = `
-010-1234-5678
+010-1234-5678.
 thesecon@gmail.com
 http://www.omdbapi.com/?apikey=70ccc60c&s=frozen
 The quick brown fox jumps over the lazy dog.
@@ -9,7 +9,4 @@ abbcccdddd
 // const regexp = new RegExp('the','gi')
 // console.log(str.match(regexp)) // (3) ['the', 'The', 'the']
 
-const regexp = /fox/gi
-// console.log(regexp.test(str)) 
-str = str.replace(regexp,'AAA')
-console.log(str)
+console.log(str.match(/\.$/gim))
