389d20e43b07ec74766d1406b8c2df43602e0231
mil2ne <show3070@gmail.com>
2022-02-21 20:27:24 +0900

    regexp practice-6

--- main.js
+++ main.js
@@ -5,16 +5,26 @@ https://www.omdbapi.com/?apikey=70ccc60c&s=frozen
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
 http://localhost:1234
-동해물과 백두산이 마르고 닳도록
+동해물과_백두산이 마르고 닳도록
 `
 
+const h = `  the hello  world   !
 
+`
 
 console.log(
-  str.match(/[가-힣]{1,}/g)
+  str.match(/\w{1,}/g)
   )
 
 console.log(
-  str.match(/[0-9]{1,}/g)
+  str.match(/\d{1,}/g)
+  )
+
+console.log(
+  str.match(/\bf\w{1,}\b/g)
+  )
+
+console.log(
+  h.replace(/\s/g,'')
   )
 
